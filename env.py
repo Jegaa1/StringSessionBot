23c7f5981ff6e4908435f310b977eefdfe3da4ac
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-28 23:37:40 +0530

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+API_ID = os.getenv("API_ID", "1738777").strip()
+API_HASH = os.getenv("API_HASH", "028f44575fc5e5ca13bdeb0b7b3f603d").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "5796564520:AAGU5KcNB1xowytuOOGuSx4TWz8jpibzXdo").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.vhu3d.mongodb.net/?retryWrites=true&w=majority").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "AsuranMoviefinder")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
